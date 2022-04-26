264e19bc7296755210af7d0ebb5d7d1dadfc702f
yarikyarichek <yarikyarichek24@icloud.com>
2022-04-26 17:10:05 +0300

    321

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"text/template"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+type Article struct {
+	Id                     uint16
+	Title, Anons, FullText string
+}
+
+var posts = []Article{}
 
 func index(w http.ResponseWriter, r *http.Request) {
 
@@ -14,7 +24,31 @@ func index(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, err.Error())
 	}
 
-	t.ExecuteTemplate(w, "index", nil)
+	db, err := sql.Open("mysql", "yara24:password@tcp(127.0.0.1:3306)/golang")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	res, err := db.Query("SELECT * FROM `articles`")
+
+	if err != nil {
+		panic(err)
+	}
+
+	posts = []Article{}
+
+	for res.Next() {
+		var post Article
+		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText)
+
+		posts = append(posts, post)
+
+	}
+
+	t.ExecuteTemplate(w, "index", posts)
 }
 
 func create(w http.ResponseWriter, r *http.Request) {
@@ -32,9 +66,39 @@ func features(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Other features")
 }
 
+func save_article(w http.ResponseWriter, r *http.Request) {
+	title := r.FormValue("title")
+	anons := r.FormValue("anons")
+	full_text := r.FormValue("full_text")
+
+	if title == "" || anons == "" || full_text == "" {
+		fmt.Fprintf(w, "Не все данные заполнены")
+	} else {
+
+		db, err := sql.Open("mysql", "yara24:password@tcp(127.0.0.1:3306)/golang")
+
+		if err != nil {
+			panic(err)
+		}
+
+		defer db.Close()
+
+		insert, err := db.Query(fmt.Sprintf("INSERT INTO `articles` (`title`, `anons`, `full_text`) VALUES ('%s', '%s', '%s')", title, anons, full_text))
+
+		if err != nil {
+			panic(err)
+		}
+
+		defer insert.Close()
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+}
+
 func handleRequest() {
 
 	http.HandleFunc("/features", features)
+	http.HandleFunc("/save_article", save_article)
 	http.HandleFunc("/create", create)
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8080", nil)
