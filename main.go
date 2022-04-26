483dcdb633056214304a40d51340b4d02c36ab23
yarikyarichek <yarikyarichek24@icloud.com>
2022-04-26 18:43:48 +0300

    7777

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"text/template"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
 type Article struct {
@@ -15,6 +16,7 @@ type Article struct {
 }
 
 var posts = []Article{}
+var showPost = Article{}
 
 func index(w http.ResponseWriter, r *http.Request) {
 
@@ -95,12 +97,56 @@ func save_article(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func show_post(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	t, err := template.ParseFiles("templates/show.html", "templates/header.html", "templates/footer.html")
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	db, err := sql.Open("mysql", "yara24:password@tcp(127.0.0.1:3306)/golang")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	res, err := db.Query(fmt.Sprintf("SELECT * FROM `articles` WHERE `id` = '%s'", vars["id"]))
+
+	if err != nil {
+		panic(err)
+	}
+
+	showPost = Article{}
+
+	for res.Next() {
+		var post Article
+		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText)
+
+		showPost = post
+
+	}
+
+	t.ExecuteTemplate(w, "show", showPost)
+
+}
+
 func handleRequest() {
 
+	rtr := mux.NewRouter()
+
+	rtr.HandleFunc("/save_article", save_article).Methods("POST")
+	rtr.HandleFunc("/create", create).Methods("GET")
+	rtr.HandleFunc("/", index).Methods("GET")
+	rtr.HandleFunc("/post/{id:[0-9]+}", show_post).Methods("GET")
+
+	http.Handle("/", rtr)
+
 	http.HandleFunc("/features", features)
-	http.HandleFunc("/save_article", save_article)
-	http.HandleFunc("/create", create)
-	http.HandleFunc("/", index)
+
 	http.ListenAndServe(":8080", nil)
 
 }
